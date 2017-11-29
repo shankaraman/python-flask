7bd8aba4a372e885a42d743a10def3e2cc53454e
Shankar Raman <s.ravindran@spee.it>
2017-11-29 17:16:03 +0100

    v1.9: conditional blocks, loops

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,7 +1,32 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = {'nickname' : 'Miguel'}
+    posts = [
+        {
+            'author' : {'nickname' : 'John'},
+            'body' : 'John is wicked witch'
+        },
+        {
+            'author' : {'nickname' : 'h1dd3ntru7h'},
+            'body' : 'Beautiful day in Italy'
+        }
+    ]
+    """return '''
+    <html>
+        <head>
+            <title> Home Page </title>
+        </head>
+        <body>
+            <h1> Hello, ''' + user['nickname'] + ''' </h1>
+        </body>
+    </html> 
+    '''"""
+    return render_template('index.html',
+                           title='Home',
+                           user=user,
+                           posts=posts)
