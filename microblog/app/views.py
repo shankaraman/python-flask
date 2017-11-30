f1ce2476f2bf708124d8eb8eb41492d050295b48
Shankar Raman <s.ravindran@spee.it>
2017-11-30 12:27:32 +0100

    v3: Authentication with OpenID, flash, forms

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,32 +1,17 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import LoginForm
 
 
-@app.route('/')
-@app.route('/index')
-def index():
-    user = {'nickname' : 'Miguel'}
-    posts = [
-        {
-            'author' : {'nickname' : 'John'},
-            'body' : 'John is wicked witch'
-        },
-        {
-            'author' : {'nickname' : 'h1dd3ntru7h'},
-            'body' : 'Beautiful day in Italy'
-        }
-    ]
-    """return '''
-    <html>
-        <head>
-            <title> Home Page </title>
-        </head>
-        <body>
-            <h1> Hello, ''' + user['nickname'] + ''' </h1>
-        </body>
-    </html> 
-    '''"""
-    return render_template('index.html',
-                           title='Home',
-                           user=user,
-                           posts=posts)
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID={}, remember_me={}'.format(
+            form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form,
+                           providers=app.config['OPENID_PROVIDERS'])
